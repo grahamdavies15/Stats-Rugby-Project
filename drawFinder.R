6a3b91006cc36fd520532acb0c585fc311bd5d59
grahamdavies15 <86721524+grahamdavies15@users.noreply.github.com>
2021-10-05 12:17:33 +0200

    Add files via upload

--- drawFinder.R
+++ drawFinder.R
@@ -1,7 +1,13 @@
 #Finding draws:
 load("rc14.Rdata")
-dat = rc14
+load("sixn1415.Rdata")
+load("euro1415.Rdata")
+load("hc1314.Rdata")
+load("sr15.Rdata")
 
+
+dat = rbind(rc14, sixn1415, euro1415, hc1314, sr15)
+            
 games = unique(dat$fx_id)
 drawFix = c()
 fixDat = NA
@@ -18,3 +24,19 @@ for (i in 1:length(games))
 }
 
 drawFix = unique(drawFix)
+
+## Use the win lose spreadsheet
+
+library(readxl)
+path <- "winlose.xlsx"
+sheetnames <- excel_sheets(path)
+winlose <- lapply(excel_sheets(path), read_excel, path = path)
+winlose <- data.frame(winlose)
+for (i in 1:length(drawFix))
+{
+  winlose <- winlose[-which(winlose$fixID == drawFix[i]),]
+}
+
+
+
+
